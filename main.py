89ca2153e2e811cd264f6b61f107245f37992fe2
Michael <michael_3@outlook.com>
2021-01-23 01:27:02 -0500

    Guarda letras en una variable

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 from pynput import keyboard
 
+palabra = []
+text = ""
+item = ""
+
 
 def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+    # palabra.append(key.char)
+    global text
+     #   text += str(key.char)
+    if str(key) == "Key.enter":
+        text+="\n"
+    elif str(key) == "Key.space":
+        text+=" "
+    elif str(key) == "Key.backspace":
+        text=text[:-1]
+    elif str(key) == "Key.up":
+        print("%flecha arriba%")
+    elif str(key) == "Key.down":
+        print("%flecha abajo%")
+    elif str(key) == "Key.left":
+        print("%flecha izquierda%")
+    elif str(key) == "Key.right":
+        print("%flecha derecha%")
+    elif str(key) == "Key.esc" or str(key) == "Key.shift_r" or str(key) == "Key.shift" or str(key) == "Key.alt_gr" or str(key) == "Key.ctrl_l" :
+        pass
+
+    else:
+        print(key)
+        text+=key.char
 
 
 def on_release(key):
@@ -16,28 +37,12 @@ def on_release(key):
         return False
 
 
-def on_activate_i_tilde():
-    print("se presionó: í")
-
-
-def for_canonical(f):
-    return lambda k: f(l.canonical(k))
-
-
-hotkey_ti = keyboard.HotKey(
-    keyboard.HotKey.parse("´+i"), on_activate_i_tilde()
-)
-
-
 # Collect events until released
 with keyboard.Listener(
         on_press=on_press,
-        on_release=on_release) as listener:
+        on_release=on_release,
+) as listener:
     listener.join()
+f = "-"
 
-with keyboard.GlobalHotKeys({
-        '´+i': on_activate_i_tilde,
-        }) as h:
-    h.join()
-
-listener.start()
+print("Al final sale:\n{0}".format(text))
